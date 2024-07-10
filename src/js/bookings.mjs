d0f61957ad59bb003e8142c7a3718b542ddf161b
TyroneMartin <tyrone306@byui.edu>
2024-07-10 10:57:41 -0600

    update to styling

--- src/js/bookings.mjs
+++ src/js/bookings.mjs
@@ -1,13 +1,13 @@
 const searchTemplate = `
 <form id="searchForm">
     <div class="search-box">
-        <input type="text" id="location" placeholder="Enter State?" required>
-        <input type="date" id="checkInDate" placeholder="Check-in Date" required>
-        <input type="date" id="checkOutDate" title="Check-out Date" required>
-        <input type="number" id="adults" placeholder="Adults" min="1" value="1" required>
-        <input type="number" id="children" placeholder="Children" min="0" value="0">
-        <input type="number" id="rooms" placeholder="Rooms" min="1" value="1" required>
-        <button type="submit">Search</button>
+    <input type="text" id="location" placeholder="Where are you going?" required>
+    <input type="date" id="checkInDate" placeholder="Check-in Date" required>
+    <input type="date" id="checkOutDate" placeholder="Check-out Date" required>
+    <input type="number" id="adults" placeholder="Enter # of Adults" min="1" required>
+    <input type="number" id="children" placeholder="Children" min="0">
+    <input type="number" id="rooms" placeholder="Enter # of Rooms" min="1" required>
+    <button type="submit">Search</button>
     </div>
 </form>
 `;
