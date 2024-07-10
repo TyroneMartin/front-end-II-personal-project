ae6a6731b378db15e34bbabbc4e4ea7238092448
TyroneMartin <tyrone306@byui.edu>
2024-07-10 10:31:24 -0600

    update

--- src/js/bookings.mjs
+++ src/js/bookings.mjs
@@ -4,9 +4,9 @@ const searchTemplate = `
         <input type="text" id="location" placeholder="Enter State?" required>
         <input type="date" id="checkInDate" placeholder="Check-in Date" required>
         <input type="date" id="checkOutDate" title="Check-out Date" required>
-        <input type="number" id="adults" placeholder="Adults" min="1" required>
-        <input type="number" id="children" placeholder="Children" min="0">
-        <input type="number" id="rooms" placeholder="Rooms" min="1" required>
+        <input type="number" id="adults" placeholder="Adults" min="1" value="1" required>
+        <input type="number" id="children" placeholder="Children" min="0" value="0">
+        <input type="number" id="rooms" placeholder="Rooms" min="1" value="1" required>
         <button type="submit">Search</button>
     </div>
 </form>
